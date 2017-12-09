4b5668c2f62c663bd7168c38c3c445a70c327606
jianglong199 <jianglonged@qq.com>
2017-12-09 23:25:47 +0800

    注册登录修改

--- register_login/login.php
+++ register_login/login.php
@@ -1,25 +1,30 @@
 <html>
+	<h1>用户登录</h1>
 	<form action="login.php" method="post">
 		用户名：<input type="text" name="user_name" /><br/>
 		密码：<input type="password" name="password" /><br/>
-		<input type="button" value="登录" />
+		<input type="submit" value="登录" />
 	</form>
 </html>
 <?php
+	if($_SERVER['REQUEST_METHOD'] == 'GET'){
+		exit;
+	}
 	$user_name = $_POST['user_name'];
 	$password = $_POST['password'];
 	//链接数据库
-	$connect = mysql_connect('127.0.0.1','root','');
+	$connect = mysql_connect('127.0.0.1','root','123456jl');
 	if(!$connect){
 		echo 'fail：连接数据库失败!';
+		exit;
 	}
-	mysql_select_db('user',$conncet);
-	$sql = "select * from user where user_name='".$user_name."' password='".$password."' limit 0,1";
+	mysql_select_db('user',$connect);
+	$sql = "select * from user where user_name='".$user_name."' and password='".$password."' limit 0,1";
 	$rsl = mysql_query($sql,$connect);
 	$user = mysql_fetch_assoc($rsl);
 	if(!$user){
-		echo "fail：用户名或密码错误！"
+		echo "fail：用户名或密码错误！";
 	}else{
-		echo "success：登录成功！"
+		echo "success：登录成功！";
 	}
 ?>
--- register_login/register.php
+++ register_login/register.php
@@ -1,12 +1,15 @@
 <html>
 	<h1>用户注册页面</h1>
-	<form action="login.php" method="post">
+	<form action="register.php" method="post">
 		用户名：<input type="text" name="user_name" /><br/>
 		密码：<input type="password" name="password" /><br/>
 		<input type="submit" value="注册">
 	</form>
 </html>
 <?php
+	if($_SERVER['REQUEST_METHOD'] == 'GET'){
+		exit;
+	}
 	$user_name = $_POST['user_name'];
 	$password = $_POST['password'];
 	if($user_name == ''){
@@ -16,7 +19,7 @@
 		echo "fail：用户名不能为空！";
 	}
 	//链接数据库
-	$connect = mysql_connect('127.0.0.1','root','');
+	$connect = mysql_connect('127.0.0.1','root','123456jl');
 	mysql_select_db('user',$connect);
 	if(!$connect){
 		die("链接数据库失败".mysql_error());
@@ -26,10 +29,12 @@
 		$resource = mysql_query($sql);
 		if($resource === false){
 			echo "fail：注册失败!";
+			exit;
 		}
 		$user=mysql_fetch_assoc($resource);
-		if($user ！== false){
+		if($user !== false){
 			echo "fail:用户名已经存在！";
+			exit;
 		}
 		$sql = "insert into user(user_name,password) values ('".$user_name."','".$password."')";
 		$rsl = mysql_query($sql);
